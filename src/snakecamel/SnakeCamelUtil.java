f7e16e18a8aa43665fe9f7f5a691300a31ff87f0
ne-san <[g16940sm@gm.tsuda.ac.jp]>
2018-05-01 17:56:33 +0900

    JUnitを利用しテストケースを作成、エラーを修正

--- src/snakecamel/SnakeCamelUtil.java
+++ src/snakecamel/SnakeCamelUtil.java
@@ -5,7 +5,7 @@ public class SnakeCamelUtil {
 	public static String snakeToCamelcase(String snake_case) {
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length ; i++) {
 			sb.append(capitalize(words[i]));
 		}
 		return new String(sb);
@@ -15,28 +15,28 @@ public class SnakeCamelUtil {
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
 		for (int i = 0; i < camelcase.length(); i++) {
-			char c = camelcase.charAt(i);
-			if (Character.isUpperCase(c)) {
-				sb.append(camelcase.substring(j, i));
+			char c = camelcase.charAt(i); //i番目の文字をcに代入
+			if (Character.isUpperCase(c)) { //もしcが大文字なら
+				sb.append(camelcase.substring(j, i)); //j番目からi番目までの文字列を取得、sbに追加
 				if (sb.length() > 0) {
 					sb.append("_");
 				}
-				sb.append(Character.toLowerCase(c));
-				j = i;
+				sb.append(Character.toLowerCase(c)); //cを小文字にしてsbに追加
+				j = i+1;
 			}
 		}
-		sb.append(camelcase.substring(j));
+		sb.append(camelcase.substring(j)); //j番目から最後までの文字列を取得
 		return new String(sb);
 	}
 	
-	static String capitalize(String s) {
+	public static String capitalize(String s) {
 		char first = s.charAt(0);
 		char upperFirst = Character.toUpperCase(first);
 		String rest = s.substring(1);
 		return upperFirst + rest;
 	}
 
-	static String uncapitalize(String s) {
+	public static String uncapitalize(String s) {
 		char first = s.charAt(0);
 		char lowerFirst = Character.toLowerCase(first);
 		String rest = s.substring(1);
